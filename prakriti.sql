b797c26d449dbed78f4372d9c182a82554a3f2cd
Prakriti-Vsk <prakritivish09@gmail.com>
2024-08-14 13:44:54 +0530

    Add files via upload
    HIIIII

---
+++ prakriti.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{2277BCE0-FB02-4FE5-ACF5-7646CAF5FCD1}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {D7C83BA9-A529-4807-AD5C-EBD08A9A494F}
+	EndGlobalSection
+EndGlobal
